a5d5dddb56fc8d735b9ff688a0a76eb41eb8fc1f
Martin Rivas Carranza <martinrivascarranza@gmail.com>
2024-07-11 10:31:43 +0100

    avances del curso

---
+++ clase_4/ecommerce/my-react-app/src/components/Producto.jsx
@@ -0,0 +1,15 @@
+function Producto(props){
+  return (
+    <div className="product">
+      <img
+        src={props.imagen}
+        alt={props.nombre}
+      />
+      <h2>{props.nombre}</h2>
+      <p>{props.precio}</p>
+      <button>Añadir al carrito</button>
+    </div>
+  );
+}
+
+export default Producto;
--- clase_4/ecommerce/my-react-app/src/main.jsx
+++ clase_4/ecommerce/my-react-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/*import App from './App.jsx'*/
-import App from './AppBoostrap.jsx'
-//import './index.css'
+import App from './App.jsx'
+//import App from './AppBootsrap'
+import './index.css'
+import CounterApp from './CounterApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   
+    <CounterApp/>
   </React.StrictMode>,
 )
