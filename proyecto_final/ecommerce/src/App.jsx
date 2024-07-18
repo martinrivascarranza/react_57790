e546783e5e2abf0ad091de502b818e638e2a77b1
Martin Rivas Carranza <martinrivascarranza@gmail.com>
2024-07-18 20:19:12 +0100

    Routes*

--- proyecto_final/ecommerce/src/App.jsx
+++ proyecto_final/ecommerce/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
@@ -8,11 +8,11 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
+      <Routes>
         <Route exact path="/" component={() => <ItemListContainer greeting="Welcome to the MLB Store!" />} />
         <Route exact path="/category/:categoryId" component={ItemListContainer} />
         <Route exact path="/item/:itemId" component={ItemDetailContainer} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
